ef29ba7f8bc399accc7ff5adbd331ed1342f5f48
Camelia Hanes <ch.cameliahanes@gmail.com>
2023-04-29 15:12:19 +0300

    Add skeleton for registration page.

--- authenticate/views.py
+++ authenticate/views.py
@@ -24,3 +24,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, ('You have been logged out.'))
     return redirect('home')
+
+
+def register_user(request):
+    return render(request, 'authenticate/register.html', {})
