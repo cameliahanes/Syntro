8d6476bd43be50b3796413c421fb0e4f3acbbfe9
Camelia Hanes <ch.cameliahanes@gmail.com>
2023-04-29 14:55:48 +0300

    Login system added to the app.

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, 'authenticate/home.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in.'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error logging in. Please try again.'))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
